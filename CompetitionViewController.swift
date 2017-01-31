34f98cdee7c85cdf413ae15fdd8675af015e3c0a
Marko Heijnen <info@markoheijnen.com>
2017-01-31 16:36:17 +0100

    Change basic views and add CompetitionViewController

--- CompetitionViewController.swift
+++ CompetitionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  CompetitionViewController.swift
 //  handbalapp
 //
 //  Created by Marko Heijnen on 1/29/17.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class CompetitionViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
--- handbalapp/handbalapp/HomeViewController.swift
+++ handbalapp/handbalapp/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  handbalapp
 //
 //  Created by Marko Heijnen on 1/29/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
