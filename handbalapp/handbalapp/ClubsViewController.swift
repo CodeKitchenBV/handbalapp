caf4363d1e50f647247601238c9dc2dfd9e08f4f
Marko Heijnen <info@markoheijnen.com>
2017-02-01 01:19:19 +0100

    Defined Navbar items with classes. See #6

---
+++ handbalapp/handbalapp/ClubsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ClubsViewController.swift
+//  handbalapp
+//
+//  Created by Marko Heijnen on 1/29/17.
+//  Copyright © 2017 CodeKitchen B.V. All rights reserved.
+//
+
+import UIKit
+
+class ClubsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
