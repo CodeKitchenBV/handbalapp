53047ce9702e620d2d7a882b4efb785a27fba82c
Marko Heijnen <info@markoheijnen.com>
2017-02-11 18:04:03 +0100

    Have image full width and smaller title. See #2

--- handbalapp/handbalapp/NewsItemController.swift
+++ handbalapp/handbalapp/NewsItemController.swift
@@ -22,22 +22,26 @@ class NewsItemController: UIViewController, UIWebViewDelegate {
 
         title = "News"
         
-        var content = "<h1>" + item.title + "</h1>" + item.content;
+        var content = "<div style='margin: 10px'><h2 style='margin: 0'>" + item.title + "</h2>" + item.content + "</div>";
 
         cache.get(NSURL(string: item.image!)! as URL).onSuccess { value in
             let imageData = NSData(data: UIImagePNGRepresentation(value)!).base64EncodedString()
             
             content = "<img src='data:image/png;base64,\(imageData)' style='max-width:100%' />" + content
-            self.webView.loadHTMLString(content, baseURL: nil)
+            self.setWebViewContent(content: content)
         }
 
+        setWebViewContent(content: content)
+    }
+    
+    func setWebViewContent(content: String) {
+        let content = "<body style='margin: 0; padding: 0'>" + content + "</body>"
         webView.loadHTMLString(content, baseURL: Bundle.main.bundleURL)
     }
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
         webView.frame.size.height = 1.0
         webView.sizeToFit()
-        
-        
     }
+
 }
