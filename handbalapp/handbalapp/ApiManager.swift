6a3629d43fe58b128ed643a86ccc7b2414050196
Marko Heijnen <info@markoheijnen.com>
2017-02-11 15:42:53 +0100

    No more network debugging of Moya. See #2

--- handbalapp/handbalapp/ApiManager.swift
+++ handbalapp/handbalapp/ApiManager.swift
@@ -9,18 +9,7 @@
 import Foundation
 import Moya
 
-private func JSONResponseDataFormatter(_ data: Data) -> Data {
-    do {
-        let dataAsJSON = try JSONSerialization.jsonObject(with: data)
-        let prettyData =  try JSONSerialization.data(withJSONObject: dataAsJSON, options: .prettyPrinted)
-        return prettyData
-    } catch {
-        return data // fallback to original data if it can't be serialized.
-    }
-}
-
-let HandbalProvider = MoyaProvider<HandbalService>(plugins: [NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter)])
-
+let HandbalProvider = MoyaProvider<HandbalService>()
 
 enum HandbalService {
     case getNews
